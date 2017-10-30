445f39867b63c17c999801267318251c40a60403
Siddharth Nayak <siddharthnayak98@gmail.com>
2017-10-31 01:40:23 +0530

    Update CU.v

--- CU.v
+++ CU.v
@@ -20,17 +20,28 @@
 //////////////////////////////////////////////////////////////////////////////////
 module CU(addr,number,opcode,addr1,addr2,addr3);
 input [31:0] addr;
+output [15:0] data_bus;
 output [3:0] opcode;
 output [4:0] addr1;
-output [4:0] addr2;
-output [4:0] addr3;
-output [7:0] number;
+output read;
+output write;
+output load_immediate;
 
-assign opcode[3:0]=addr[31:28];
+//output [4:0] addr2;
+//output [4:0] addr3;
+//output [7:0] number;
+
+assign data_bus=addr[31:16];
+assign addr1=addr[15:11];
+assign read=addr[10];
+assign write=addr[9];
+assign load_immediate=addr[8];
+assign opcode=addr[7:4];
+assign flag_register=addr[3:0];
+/*assign opcode[3:0]=addr[31:28];
 assign number[7:0]=addr[22:15];
 assign addr3[4:0]= addr[14:10];
 assign addr2[4:0]=addr[9:5];
-assign addr1[4:0]=addr[4:0];
-
+assign addr1[4:0]=addr[4:0];*/
 
 endmodule
