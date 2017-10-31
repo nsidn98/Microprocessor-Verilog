63ca630e9b80a1d3dcd3acdcb1ea40b426965c4f
Siddharth Nayak <siddharthnayak98@gmail.com>
2017-10-31 12:01:15 +0530

    Update execute.v

--- execute.v
+++ execute.v
@@ -1,30 +1,34 @@
 module execute(instruction);
-input [31:0] instruction;
-wire [31:0] reg_instruction;
-wire [15:0] reg_data_bus;
+  input [52:0] instruction;
+  wire [52:0] reg_instruction;
+wire [15:0] reg_data_bus_1;
+wire [15:0] reg_data_bus_2;
 wire [4:0] reg_addr1;
 wire [4:0] reg_addr2;
 wire [4:0] reg_addr3;
 wire [15:0] reg_out;
-wire reg_read;
-wire reg_write;
 wire reg_opcode_enable;
+wire reg_load_immediate;
+wire reg_read_write;
 wire [3:0] reg_opcode;
 wire [3:0] reg_flag_register;
-reg opcode_enable;
-integer count=0;
+wire [15:0] reg_out;
+output [15:0] out;
+output [3:0] flag;
 
-CU CU_1(.addr(reg_instruction),.data_bus(reg_data_bus),.opcode(reg_opcode),.addr1(reg_addr1),.read(reg_read),.write(reg_write),.opcode_enable(reg_opcode_enable),.flag_register(reg_flag_register));
+  CU CU_1(.addr(reg_instruction),.data_bus_1(reg_data_bus_1),.data_bus_2(reg_data_bus_2),.opcode(reg_opcode),.addr1(reg_addr1),.addr2(reg_addr2),.addr3(reg_addr3),.load_immediate(reg_load_immediate),.read_write(reg_read_write));
+//a,b,opcode,flag,out,addr1,addr2,addr3
+  EU EU_1(.a(reg_data_bus_1),.b(reg_data_bus_2),.opcode(reg_opcode),.flag(reg_flag_register),.out(reg_out),.addr1(reg_addr1),.addr2(reg_addr2),.addr3(reg_addr3));
+//input example:0000000000000010-0000000000000001-0000-00000-00001-00010-1-0;
+//memory MEMORY_1(.data_in(temp_in_1),.data_out(temp_out_1),.read_write(c),.addr(temp_addr));
+//top_model ALU_1(.opcode(opcode),.a(a),.b(b),.flag(flag),.out(out));
 
-EU EU_1();
+  always @(instruction)
+    begin
+      assign reg_instruction=instruction;
+      assign output =reg_out;
+      assign flag=reg_flag;
+    end  
 
-memory MEMORY_1(.data_in(temp_in_1),.data_out(temp_out_1),.read_write(c),.addr(temp_addr));
-top_model ALU_1(.opcode(opcode),.a(a),.b(b),.flag(flag),.out(out));
-
-
-always @(posedge opcode_enable)
-begin
-
-end
 
 endmodule
